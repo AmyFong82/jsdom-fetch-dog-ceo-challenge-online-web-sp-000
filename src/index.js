a0b5a507deb411bf418ca6ebea39afe4f9457e80
AmyFong82 <amyhfong@gmail.com>
2021-02-04 00:40:58 -0500

    commit tree 03b204511ae2c5b75dd2458cf2da771c81e3d52e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchDogImages();
+})
+
+function fetchDogImages() {
+	fetch(imgUrl)
+	  .then(resp => resp.json())
+	  .then(json => renderDogImages(json));
+}
+
+
+function renderDogImages(json){
+	const imageContainer = document.querySelector('#dog-image-container')
+	json.message.forEach(dogImageURL => {
+	  	imageContainer.innerHTML += `<img src="${dogImageURL}" width="400" height="300">`
+	})
+}
+
+
+
+// fetch('https://dog.ceo/api/breeds/image/random/4')
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(json) {
+//   json.message.forEach(dogImageURL => {
+//     const img = document.createElement('img')
+//     img.innerHTML = `<img src=${dogImageURL}>`
+//     document.getElementsByTagName("body").appendChild(img)
+//   })
+// });
+
+
